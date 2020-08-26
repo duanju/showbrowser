26b68c074cb3911e96f7d775639e9334885bb06f
Ju Duan <duanju91@163.com>
2020-08-26 20:22:15 +0800

    feat: when pressing back button in browser page, we will be nev to search page

--- app/src/main/java/com/dhms/tvshow/BrowserActivity.java
+++ app/src/main/java/com/dhms/tvshow/BrowserActivity.java
@@ -1,10 +1,14 @@
 package com.dhms.tvshow;
 
+import android.content.Intent;
 import android.text.TextUtils;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.FragmentActivity;
 
 /**
@@ -13,6 +17,8 @@ import androidx.fragment.app.FragmentActivity;
  */
 public class BrowserActivity extends FragmentActivity {
     public static final String URL = "Browser_Activity_URL";
+
+    private Dpad mDpad = new Dpad();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,17 @@ public class BrowserActivity extends FragmentActivity {
                     , "Please specific URL", Toast.LENGTH_SHORT).show();
             finish();
         }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            Intent startSearchPage = new Intent(this, SearchActivity.class);
+            startActivity(startSearchPage);
+            finish();
+            return false;
+        }
+
+        return super.onKeyUp(keyCode, event);
     }
 }
